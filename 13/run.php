b2b4b7a2e9fa8033c81987177502aec7294227b7
Anatoliy Yudov <anatoliy.yudov@supermetrics.com>
2024-12-13 07:52:02 +0200

    refactor

--- 13/run.php
+++ 13/run.php
@@ -3,42 +3,38 @@
 $input = file('input.txt', FILE_IGNORE_NEW_LINES);
 
 $rules = [];
+
 for($i = 0; $i < count($input); $i += 4) {
     list($d1, $d2, $xv, $yv) = explode(' ', $input[$i]);
     $a = [explode('+', substr($xv, 0, -1))[1], explode('+', $yv)[1]];
-
     list($d1, $d2, $xv, $yv) = explode(' ', $input[$i + 1]);
     $b = [explode('+', substr($xv, 0, -1))[1], explode('+', $yv)[1]];
-
     list($d, $xv, $yv) = explode(' ', $input[$i + 2]);
     $p = [explode('=', substr($xv, 0, -1))[1], explode('=', $yv)[1]];
-
-    $rules[] = [
-        'a' => $a, 'b' => $b, 'prize' => $p
-    ];
+    $rules[] = ['a' => $a, 'b' => $b, 'prize' => $p];
 }
 
 $ans1 = $ans2 = 0;
 
 foreach($rules as $rule) {
+    $ans1 += calc($rule, false);
+    $ans2 += calc($rule, true);
+}
+
+printf('First star: %s%sSecond star: %s%s', $ans1, PHP_EOL, $ans2, PHP_EOL);
+
+function calc($rule, $part2 = false): int
+{
+    if ($part2) {
+        $rule['prize'][0] += 10000000000000;
+        $rule['prize'][1] += 10000000000000;
+    }
+
     $a = ($rule['prize'][0] * $rule['b'][1] - $rule['b'][0] * $rule['prize'][1]) /
         ($rule['a'][0] * $rule['b'][1] - $rule['b'][0] * $rule['a'][1]);
-    if (!is_int($a)) continue;
     $b = ($rule['prize'][0] - $rule['a'][0] * $a) / $rule['b'][0];
-    if (!is_int($b)) continue;
-    $ans1 += $a * 3 + $b;
-}
-echo $ans1 . PHP_EOL;
 
+    if ((!is_int($a)) || (!is_int($b))) return 0;
 
-foreach($rules as $rule) {
-    $rule['prize'][0] += 10000000000000;
-    $rule['prize'][1] += 10000000000000;
-    $a = ($rule['prize'][0] * $rule['b'][1] - $rule['b'][0] * $rule['prize'][1]) /
-        ($rule['a'][0] * $rule['b'][1] - $rule['b'][0] * $rule['a'][1]);
-    if (!is_int($a)) continue;
-    $b = ($rule['prize'][0] - $rule['a'][0] * $a) / $rule['b'][0];
-    if (!is_int($b)) continue;
-    $ans2 += $a * 3 + $b;
+    return $a * 3 + $b;
 }
-echo $ans2 . PHP_EOL;
